287a9b0b5ffd7d3daeaee35e90e6c88d3ac9aa65
Rachel Perez <reperez@me.com>
2019-12-30 12:53:37 -0500

    setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'practice',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'http://ec2-52-15-70-108.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/r.pem',
+        ref: 'origin/master',
+        repo: 'https://github.com/reperez120/practice.git',
+        path: '/home/ubuntu/practice',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
